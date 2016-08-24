f79035189be31fed881d4ee1bf328cead0e43df0
Darcy Beurle <dbeurle@au1.ibm.com>
2016-08-25 08:53:33 +1000

    Added enumerations for each available element type

--- src/GmshReader.hpp
+++ src/GmshReader.hpp
@@ -53,7 +53,41 @@ public:
     ~GmshReader() = default;
 
     /** Gmsh element numbering scheme */
-    enum { LINE2 = 1, TRI3, QUAD4, TETRA4, HEX8, PRISM6, TRI6 = 9, TETRA10 = 11};
+    enum ELEMENT_TYPE_ID {// Standard linear elements
+                          LINE2=1,
+                          TRIANGLE3,
+                          QUADRILATERAL4,
+                          TETRAHEDRON4,
+                          HEXAHEDRON8,
+                          PRISM6,
+                          PYRAMID5,
+                          // Quadratic elements
+                          LINE3,
+                          TRIANGLE6,
+                          QUADRILATERAL9,  // 4 vertex, 4 edges and 1 face node
+                          TETRAHEDRON10,
+                          HEXAHEDRON27,
+                          PRISM18,
+                          PYRAMID14,
+                          POINT = 15,
+                          QUADRILATERAL8,
+                          HEXAHEDRON20,
+                          PRISM15,
+                          PYRAMID13,
+                          TRIANGLE9 = 20,
+                          TRIANGLE10,
+                          TRIANGLE12,
+                          TRIANGLE15,
+                          TRIANGLE15_IC,  // Incomplete 15 node triangle
+                          TRIANGLE21 = 25,
+                          EDGE4,
+                          EDGE5,
+                          EDGE6,
+                          TETRAHEDRON20,
+                          TETRAHEDRON35,
+                          TETRAHEDRON56,
+                          HEXAHEDRON64 = 92,
+                          HEXAHEDRON125};
 
     const std::map<StringKey, Value>& mesh() const {return gmshMesh;}
 
