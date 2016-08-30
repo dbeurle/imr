648e05d5ddfb182e39503ccb141c8dfe0f3138f4
Darcy Beurle <darcy@neonfe.io>
2016-08-30 18:02:30 +1000

    Changed namespace to gmsh from neon

--- examples/first_example.cpp
+++ examples/first_example.cpp
@@ -2,7 +2,7 @@
 #include "GmshReader.hpp"
 int main()
 {
-  neon::GmshReader reader("feti_beam.msh");
+  gmsh::Reader reader("feti_beam.msh");
   reader.writeMesh("mesh.neon");
   std::cout << "This is my first example" << std::endl;
 }
