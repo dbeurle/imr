7d2d7e24e353c3aa7b3fed87b3420c1d505f7fa3
phuschke <philiphuschke@gmail.com>
2016-11-18 10:10:05 +0100

    Modified an example to take the mesh file as an input argument.

--- examples/first_example.cpp
+++ examples/first_example.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "GmshReader.hpp"
-int main()
-{
-    using namespace gmsh;
 
-    gmsh::Reader reader( "feti_beam_fine.msh",
-                         Reader::NodalOrdering::Local,
-                         Reader::IndexingBase::Zero);
+using namespace gmsh;
+
+int main(int argc, char** argv)
+{
+
+
+    std::string file = argv[1];
+
+    std::cout << file << std::endl;
+
+    gmsh::Reader reader( file,
+                         Reader::NodalOrdering::Global,
+                         Reader::IndexingBase::One);
+
     reader.writeMeshToJson();
 
     std::cout << "This is my first example" << std::endl;
