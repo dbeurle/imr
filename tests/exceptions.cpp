a6220945f3eeda629df04bbbf468161527215bad
Darcy Beurle <darcy@beurle.id.au>
2017-02-01 21:00:06 +0100

    Fixed compile error with gcc-7

--- tests/exceptions.cpp
+++ tests/exceptions.cpp
@@ -1,16 +1,18 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch.hpp>
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one
+                          // cpp file
 #include "GmshReader.hpp"
 #include "GmshReaderException.hpp"
+#include <catch.hpp>
 
-TEST_CASE( "Check for exceptions", "[exceptions]" )
+using namespace gmsh;
+
+TEST_CASE("Check for exceptions", "[exceptions]")
 {
-    SECTION( "Throw a GmshReaderException for invalid mesh files" )
+    SECTION("Throw a GmshReaderException for invalid mesh files")
     {
-        using namespace gmsh;
-        REQUIRE_THROWS_AS(gmsh::Reader reader("invalid_file_name",
-                                              Reader::NodalOrdering::Global,
-                                              Reader::IndexingBase::One),
+        REQUIRE_THROWS_AS(gmsh::Reader("invalid_file_name",
+                                       Reader::NodalOrdering::Global,
+                                       Reader::IndexingBase::One),
                           GmshReaderException);
     }
 }
