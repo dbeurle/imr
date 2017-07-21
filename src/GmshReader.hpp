2784251d60bb4f308a2ba5099fb3c8e42a845015
Darcy Beurle <darcy@beurle.id.au>
2017-07-21 13:08:04 +0200

    Formatting changes

--- src/GmshReader.hpp
+++ src/GmshReader.hpp
@@ -4,12 +4,12 @@
 
 #pragma once
 
-#include <string>
-#include <vector>
 #include <array>
-#include <set>
 #include <fstream>
 #include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 #include "ElementData.hpp"
 
@@ -24,59 +24,57 @@ struct NodeData
 
 /**
  * Reader parses Gmsh format and returns the data structures of the mesh
- * in a format for neon to process
+ * in a json format for easier processing
  */
 class Reader
 {
 public:
-
     using StringKey = std::string;
     using Value     = std::vector<ElementData>;
 
 public:
-
-    enum class NodalOrdering {Local, Global};
-    enum class IndexingBase {Zero, One};
+    enum class NodalOrdering { Local, Global };
+    enum class IndexingBase { Zero, One };
 
     /** Gmsh element numbering scheme */
-    enum ELEMENT_TYPE_ID {// Standard linear elements
-                          LINE2=1,
-                          TRIANGLE3,
-                          QUADRILATERAL4,
-                          TETRAHEDRON4,
-                          HEXAHEDRON8,
-                          PRISM6,
-                          PYRAMID5,
-                          // Quadratic elements
-                          LINE3,
-                          TRIANGLE6,
-                          QUADRILATERAL9,  // 4 vertex, 4 edges and 1 face node
-                          TETRAHEDRON10,
-                          HEXAHEDRON27,
-                          PRISM18,
-                          PYRAMID14,
-                          POINT = 15,
-                          QUADRILATERAL8,
-                          HEXAHEDRON20,
-                          PRISM15,
-                          PYRAMID13,
-                          TRIANGLE9 = 20,
-                          TRIANGLE10,
-                          TRIANGLE12,
-                          TRIANGLE15,
-                          TRIANGLE15_IC,  // Incomplete 15 node triangle
-                          TRIANGLE21 = 25,
-                          EDGE4,
-                          EDGE5,
-                          EDGE6,
-                          TETRAHEDRON20,
-                          TETRAHEDRON35,
-                          TETRAHEDRON56,
-                          HEXAHEDRON64 = 92,
-                          HEXAHEDRON125};
+    enum ELEMENT_TYPE_ID { // Standard linear elements
+        LINE2 = 1,
+        TRIANGLE3,
+        QUADRILATERAL4,
+        TETRAHEDRON4,
+        HEXAHEDRON8,
+        PRISM6,
+        PYRAMID5,
+        // Quadratic elements
+        LINE3,
+        TRIANGLE6,
+        QUADRILATERAL9, // 4 vertex, 4 edges and 1 face node
+        TETRAHEDRON10,
+        HEXAHEDRON27,
+        PRISM18,
+        PYRAMID14,
+        POINT = 15,
+        QUADRILATERAL8,
+        HEXAHEDRON20,
+        PRISM15,
+        PYRAMID13,
+        TRIANGLE9 = 20,
+        TRIANGLE10,
+        TRIANGLE12,
+        TRIANGLE15,
+        TRIANGLE15_IC, // Incomplete 15 node triangle
+        TRIANGLE21 = 25,
+        EDGE4,
+        EDGE5,
+        EDGE6,
+        TETRAHEDRON20,
+        TETRAHEDRON35,
+        TETRAHEDRON56,
+        HEXAHEDRON64 = 92,
+        HEXAHEDRON125
+    };
 
 public:
-
     /**
      * @param File name of gmsh mesh
      * @param Flag to use local processor ordering or retain global ordering.
@@ -85,7 +83,7 @@ public:
               the mesh file in addition to the nodal connectivity
      * @param Flag for zero based indexing in nodal coordinates
      */
-    Reader( std::string const& fileName, NodalOrdering, IndexingBase);
+    Reader(std::string const& fileName, NodalOrdering, IndexingBase);
 
     ~Reader() = default;
 
@@ -94,13 +92,13 @@ public:
      * The physicalIds and the names are given by names().
      * The value in the map is a list of ElementData objects
      */
-    const std::map<StringKey, Value>& mesh() const {return gmshMesh;}
+    const std::map<StringKey, Value>& mesh() const { return gmshMesh; }
 
     /** Return a list of the coordinates and Ids of the nodes */
-    const std::vector<NodeData>& nodes() const {return nodeList;}
+    const std::vector<NodeData>& nodes() const { return nodeList; }
 
     /** Return the physical names associated with the mesh */
-    const std::map<int, std::string>& names() const {return physicalGroupMap;}
+    const std::map<int, std::string>& names() const { return physicalGroupMap; }
 
     void writeMesh(const std::string& outputFileName);
 
@@ -114,7 +112,6 @@ public:
     void writeMeshToJson() const;
 
 private:
-
     /**
      * Provide a reference to the nodes and dimensions that will be populated
      * with the correct data based on the elementType
@@ -143,16 +140,16 @@ private:
                           std::vector<int> const& localToGlobalMapping) const;
 
     /** Gather the local process nodes using the local to global mapping */
-    std::vector<NodeData> fillLocalNodeList(std::vector<int> const& localToGlobalMapping) const;
+    std::vector<NodeData>
+    fillLocalNodeList(std::vector<int> const& localToGlobalMapping) const;
 
-    void writeInJsonFormat( std::map<StringKey, Value> const& processMesh,
-                            std::vector<int> const& localToGlobalMapping,
-                            std::vector<NodeData> const& nodalCoordinates,
-                            int processId,
-                            bool isMeshDistributed) const;
+    void writeInJsonFormat(std::map<StringKey, Value> const& processMesh,
+                           std::vector<int> const& localToGlobalMapping,
+                           std::vector<NodeData> const& nodalCoordinates,
+                           int processId,
+                           bool isMeshDistributed) const;
 
 private:
-
     std::map<StringKey, Value> gmshMesh;
     std::map<int, std::string> physicalGroupMap;
     /**
@@ -162,16 +159,14 @@ private:
      * Value:       node ids of the interface element
      *
      */
-    std::map<std::pair<int,int>, std::set<int>> interfaceElementMap;
+    std::map<std::pair<int, int>, std::set<int>> interfaceElementMap;
 
     std::vector<NodeData> nodeList;
 
-    std::string fileName;   //!< File name of gmsh file
-    std::fstream gmshFile;  //!< Hold an object to the file stream
+    std::string fileName;  //!< File name of gmsh file
+    std::fstream gmshFile; //!< Hold an object to the file stream
 
     bool useZeroBasedIndexing;
     bool useLocalNodalConnectivity;
-
 };
-
 }
