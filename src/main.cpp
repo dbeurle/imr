a95e33175da73d68c66f6c5c786a4ff8f2266aeb
Darcy Beurle <darcy@neonfe.io>
2017-07-24 10:01:20 +0200

    Added gmsh reader with command line interface

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,53 @@
 
 int main(int argc, char* argv[])
 {
+    using namespace gmsh;
+
     // Parse the command line options and process the mesh
     try
     {
         // Define and parse the program options
         namespace po = boost::program_options;
-        po::options_description desc("Options");
 
-        desc.add_options()("help", "Print help messages");
+        po::options_description visible("Options");
 
-        desc.add_options()("zero-based",
-                           "Use zero based indexing for elements and nodes");
+        visible.add_options()("help", "Print help messages");
+        visible.add_options()("zero-based",
+                              "Use zero based indexing for elements and nodes");
+        visible.add_options()("local-ordering",
+                              "For distributed meshes, each processor has local indexing "
+                              "and a local to global mapping");
+        visible.add_options()("with-indices",
+                              "Write out extra indices (results in file size increase)");
 
-        desc.add_options()("local-ordering",
-                           "For distributed meshes, each processor has local indexing "
-                           "and a local to global mapping");
+        po::options_description hidden("Hidden options");
 
-        desc.add_options()("no-indices", "Do not write out extra indices to save space");
+        hidden.add_options()("input-file",
+                             po::value<std::vector<std::string>>(),
+                             "input file");
+
+        po::options_description cmdline_options;
+        cmdline_options.add(visible).add(hidden);
+
+        po::positional_options_description p;
+        p.add("input-file", -1);
 
         po::variables_map vm;
 
         try
         {
-            po::store(po::parse_command_line(argc, argv, desc), vm);
+            po::store(po::command_line_parser(argc, argv)
+                          .options(cmdline_options)
+                          .positional(p)
+                          .run(),
+                      vm);
 
-            // --help option
-            if (vm.count("help"))
+            if (vm.count("help") || argc < 2)
             {
                 std::cout << "\nA serial and parallel gmsh processing tool to convert "
-                             ".msh to .json files for easy input\n\n"
-                          << desc << std::endl;
+                             ".msh to .json files\n\n"
+                          << "gmshreader [Options] filename.msh\n\n"
+                          << visible << std::endl;
                 return 0;
             }
             po::notify(vm); // throws on error, so do after help in case
@@ -44,11 +61,30 @@ int main(int argc, char* argv[])
         catch (po::error& e)
         {
             std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
-            std::cerr << desc << std::endl;
+            std::cerr << visible << std::endl;
             return 1;
         }
 
-        // application code here //
+        Reader::IndexingBase indexing = vm.count("zero-based") > 1
+                                            ? Reader::IndexingBase::Zero
+                                            : Reader::IndexingBase::One;
+
+        Reader::NodalOrdering ordering = vm.count("local-ordering") > 1
+                                             ? Reader::NodalOrdering::Local
+                                             : Reader::NodalOrdering::Global;
+
+        if (vm.count("input-file"))
+        {
+            for (auto const& input : vm["input-file"].as<std::vector<std::string>>())
+            {
+                Reader reader(input, ordering, indexing);
+                reader.writeMeshToJson(vm.count("with-indices") > 1);
+            }
+        }
+        else
+        {
+            throw std::runtime_error("Missing \".msh\" input file!\n");
+        }
     }
     catch (std::exception& e)
     {
