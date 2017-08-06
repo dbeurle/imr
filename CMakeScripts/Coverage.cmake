26a0804d9574cea175fceda870636dd3385e3b2d
Darcy Beurle <darcy@beurle.id.au>
2017-08-06 17:22:44 +0200

    Removed tabs and included catch to exclude in coverage

--- CMakeScripts/Coverage.cmake
+++ CMakeScripts/Coverage.cmake
@@ -4,9 +4,10 @@ if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 endif()
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
 add_custom_target(coverage
-		COMMAND ${CMAKE_MAKE_PROGRAM} -j8
-		COMMAND ctest
-		COMMAND lcov --capture --directory ${CMAKE_BINARY_DIR} --output-file coverage.info
-		COMMAND lcov -r coverage.info '/usr/include/*' -o coverage.info
-		COMMAND genhtml coverage.info --output-directory coverageOutput
-		COMMAND xdg-open coverageOutput/index.html)
+        COMMAND ${CMAKE_MAKE_PROGRAM} -j8
+        COMMAND ctest
+        COMMAND lcov --capture --directory ${CMAKE_BINARY_DIR} --output-file coverage.info
+        COMMAND lcov -r coverage.info '/usr/include/*' -o coverage.info
+        COMMAND lcov -r coverage.info 'build/catch/*' -o coverage.info
+        COMMAND genhtml coverage.info --output-directory coverageOutput
+        COMMAND xdg-open coverageOutput/index.html)
